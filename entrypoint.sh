b4673439d25be00c0499fef34b4badb38fa1245e
vchandra22 <vincentchaandra@gmail.com>
2025-08-29 14:39:50 +0700

    fix: entrypoint to run laravel

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
-echo "🔧 Menyetel permission direktori & permission Laravel..."
+#!/bin/sh
+set -e
+
+echo "🔧 Set permission Laravel..."
 mkdir -p /app/storage/framework/{cache/data,sessions,views,testing}
 mkdir -p /app/bootstrap/cache
-
 chown -R www-data:www-data /app/storage /app/bootstrap/cache
 chmod -R ug+rwX /app/storage /app/bootstrap/cache
 
-echo "🚀 Menjalankan Laravel Octane (FrankenPHP)..."
-exec php artisan octane:start --server=frankenphp --host=0.0.0.0 --port=90 --admin-port=2019
+echo "⚡ Optimize Laravel cache..."
+php artisan config:cache || true
+php artisan route:cache || true
+php artisan view:cache || true
+
+echo "🚀 Start FrankenPHP directly..."
+exec frankenphp run --workers=4 --max-requests=500 public/index.php
